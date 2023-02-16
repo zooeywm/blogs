ad7e3380223402dd3cb34e14a71de2189a15c145
zooeywm <zooeywm@gmail.com>
2023-02-16 21:17:33 +0800

    💚(ci): Add Wechat Notification

---
+++ utils/wechat.py
@@ -0,0 +1,31 @@
+import os
+import math
+import random
+import requests
+
+from datetime import date, datetime
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+
+today = datetime.now()
+
+# 微信公众测试号 ID 和 SECRET
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+# 可把 os.environ 结果替换成字符串在本地调试
+user_ids = os.environ["USER_ID"].split(',')
+template_ids = os.environ["TEMPLATE_ID"].split(',')
+
+# 字体随机颜色
+def get_random_color():
+    return "#%06x" % random.randint(0, 0xFFFFFF)
+
+
+client = WeChatClient(app_id, app_secret)
+wm = WeChatMessage(client)
+
+for i in range(len(user_ids)):
+    data = {}
+    res = wm.send_template(user_ids[i], template_ids[i], data)
+    print(res)
